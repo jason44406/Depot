2e9a215f5abba7ac55202f455d79b19e8af4fa4e
Jason Cachia <jcachia@gannett.com>
2017-09-28 13:45:24 -0400

    Chap 9 Iter D1 - Add cart migration and module

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825222545) do
+ActiveRecord::Schema.define(version: 20170928173728) do
+
+  create_table "carts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "title"
